ca0da71e6b7dc9a9c7e64e6913eb7aeec1a63cb1
CodeSevenic <sevenicbookings@gmail.com>
2023-03-12 12:23:26 +0200

    HS Integration code update

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,42 @@
+﻿import React, { useState, useEffect } from 'react';
+import '../styles/Dashboard.css';
+
+function Dashboard() {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    async function fetchContacts() {
+      const response = await fetch('/api/contacts');
+      const data = await response.json();
+      setContacts(data);
+    }
+
+    fetchContacts();
+  }, []);
+
+  return (
+    <div>
+      <h1>Contacts</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Last Contacted</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact) => (
+            <tr key={contact.id}>
+              <td>{contact.name}</td>
+              <td>{contact.email}</td>
+              <td>{contact.lastContacted}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Dashboard;
